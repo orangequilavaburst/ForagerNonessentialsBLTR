aee134081939b42929c87a7e6f105cb267cb05f0
J8-Bit <orangequilavaburst@gmail.com>
2026-02-06 16:27:53 -0500

    agent credits

--- forager_nonessentials.lua
+++ forager_nonessentials.lua
@@ -912,6 +912,25 @@ SMODS.current_mod.extra_tabs = function()
 														},
 													}
 												},
+												{
+													n = G.UIT.R,
+													config = {
+														align = "cm"
+													},
+													nodes = {
+
+														{
+															n = G.UIT.T,
+															config = {
+																align = 'cm',
+																text = "AgentTheVandal",
+																colour = G.C.UI.TEXT_LIGHT,
+																scale = 0.25,
+																padding = 0.05
+															}
+														},
+													}
+												},
 											}
 										},
 										{
